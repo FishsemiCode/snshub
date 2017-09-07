9411e4ab81e471a1592ecb6aa867c5f8a7cc6ae5
QianWenfa <qianwenfa@xiaomi.com>
2017-09-07 18:07:49 +0800

    makefile: modify the rules.mk to add the needs sources
    and add support for TL420 toolchain
    
    Change-Id: I9bb01c14bf0754008bdc7f459970ffe66cbeeafa
    Signed-off-by: QianWenfa <qianwenfa@xiaomi.com>

--- rules.mk
+++ rules.mk
@@ -27,17 +27,20 @@ MODULE_SRCS += \
 	$(LOCAL_DIR)/sensor_manager.c \
 	$(LOCAL_DIR)/client_manager.c \
 	$(LOCAL_DIR)/circ_buffer.c \
-	$(LOCAL_DIR)/sensor_board.c \
+	$(LOCAL_DIR)/sensor_board_fimi.c \
 	$(LOCAL_DIR)/algo_manager.c \
 	$(LOCAL_DIR)/sensor_port.c \
 	$(LOCAL_DIR)/sensor_port_i2c.c \
 	$(LOCAL_DIR)/sensor_port_spi.c \
 	$(LOCAL_DIR)/host_interface.c \
 	$(LOCAL_DIR)/host_interface_rpmsg.c \
-	$(LOCAL_DIR)/android_client.c \
 	$(LOCAL_DIR)/utils.c \
-	$(LOCAL_DIR)/sensor_driver/ltr579.c \
-	$(LOCAL_DIR)/sensor_driver/yas537.c \
-	$(LOCAL_DIR)/sensor_driver/icm20690.c \
+	$(LOCAL_DIR)/sensor_driver/icm20602.c \
+
+ifeq ($(ARCH),ceva)
+EXTRA_OBJS += $(call TOBUILDDIR, $(LOCAL_DIR))/sensor_board_fimi.o
+EXTRA_OBJS += $(call TOBUILDDIR, $(LOCAL_DIR))/client_manager.o
+EXTRA_OBJS += $(call TOBUILDDIR, $(LOCAL_DIR))/sensor_driver/icm20602.o
+endif
 
 include make/module.mk
