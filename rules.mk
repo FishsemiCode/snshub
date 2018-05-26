be958ca74ffa96d3234249019976919b4b09ed1d
QianWenfa <qianwenfa@xiaomi.com>
2018-05-26 16:09:54 +0800

    for banks evb: change the board config to banks evb
    and also add drivers for icm20690 and ltr579
    
    TODO: later we will actually support dynamically binding the board
    config with the true hardware, and then we could push all the board
    configurations into build
    
    Change-Id: I5a66d2bd0b2cc5e187f27e3ebabe02a616fb24db
    Signed-off-by: QianWenfa <qianwenfa@xiaomi.com>

--- rules.mk
+++ rules.mk
@@ -23,21 +23,23 @@ MODULE_INCLUDES += \
 	$(LOCAL_DIR)/sensor_driver \
 
 MODULE_SRCS += \
-	$(LOCAL_DIR)/hw_sensors.c \
-	$(LOCAL_DIR)/sensor_manager.c \
-	$(LOCAL_DIR)/client_manager.c \
-	$(LOCAL_DIR)/circ_buffer.c \
-	$(LOCAL_DIR)/sensor_board_bumblebee.c \
 	$(LOCAL_DIR)/algo_manager.c \
+	$(LOCAL_DIR)/circ_buffer.c \
+	$(LOCAL_DIR)/client_manager.c \
+	$(LOCAL_DIR)/host_interface.c \
+	$(LOCAL_DIR)/host_interface_rpmsg.c \
+	$(LOCAL_DIR)/hw_sensors.c \
+	$(LOCAL_DIR)/sensor_board.c \
+	$(LOCAL_DIR)/sensor_driver/icm20602.c \
+	$(LOCAL_DIR)/sensor_driver/icm20690.c \
+	$(LOCAL_DIR)/sensor_driver/ltr579.c \
+	$(LOCAL_DIR)/sensor_driver/spl06.c \
+	$(LOCAL_DIR)/sensor_driver/ist8307a_imp.c \
+	$(LOCAL_DIR)/sensor_manager.c \
 	$(LOCAL_DIR)/sensor_port.c \
 	$(LOCAL_DIR)/sensor_port_i2c.c \
 	$(LOCAL_DIR)/sensor_port_spi.c \
-	$(LOCAL_DIR)/host_interface.c \
-	$(LOCAL_DIR)/host_interface_rpmsg.c \
 	$(LOCAL_DIR)/utils.c \
-	$(LOCAL_DIR)/sensor_driver/icm20602.c \
-	$(LOCAL_DIR)/sensor_driver/spl06.c \
-	$(LOCAL_DIR)/sensor_driver/ist8307a_imp.c \
 
 ifeq ($(ARCH),ceva)
 EXTRA_OBJS += $(call TOBUILDDIR, $(LOCAL_DIR))/sensor_board_bumblebee.o
